e5f0e61e65f5ee10e9da448d5a736ecee18908e0
Utilisateur <user@debian.arcahe.ovh>
2020-05-24 22:16:17 +0200

    Reorganisation

---
+++ pglp/src/main/java/lylia/App.java
@@ -0,0 +1,13 @@
+package lylia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ pglp/src/test/java/lylia/AppTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class AppTest {
+  /** Rigorous Test :-) */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
