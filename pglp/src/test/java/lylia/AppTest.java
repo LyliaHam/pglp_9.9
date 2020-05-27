40a6b7bf6f73386ef7d427f8f6212ebb9f03fd77
Utilisateur <user@debian.arcahe.ovh>
2020-05-27 23:37:38 +0200

    AppTest

--- pglp/src/test/java/lylia/AppTest.java
+++ pglp/src/test/java/lylia/AppTest.java
@@ -1,11 +1,8 @@
-import org.junit.Test;
-
 import static org.junit.Assert.assertTrue;
 
 
 public class AppTest {
-  /** Rigorous Test :-) */
-  @Test
+
   public void shouldAnswerWithTrue() {
     assertTrue(true);
   }
